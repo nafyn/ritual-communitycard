23b80352b4f68f561aee605644f7a0758b00ef23
nafyn <93058690+nafyn@users.noreply.github.com>
2025-11-03 13:25:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,22 @@ const descs = [
   "this user will follow you through your API calls"
 ];
 
+const card = document.getElementById("card");
+
+card.addEventListener("mousemove", e => {
+  const rect = card.getBoundingClientRect();
+  const x = e.clientX - rect.left - rect.width/2;
+  const y = e.clientY - rect.top - rect.height/2;
+  const rotateX = (y / rect.height) * -8;
+  const rotateY = (x / rect.width) * 8;
+  card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+});
+
+card.addEventListener("mouseleave", () => {
+  card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+});
+
+
 function getGrade(role) {
   if (["Initiate", "Ritualist Ascendant"].includes(role)) return "Common";
   if (["Ritty Bitty", "Ritty"].includes(role)) return "Rare";
